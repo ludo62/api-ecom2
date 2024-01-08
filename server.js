66ede7757552a084bc38e2586ea935b50c296186
ludo62 <fournier.ludovic@orange.fr>
2024-01-08 11:52:39 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+// Chargements des variables d'environnement
+// Import des modules necessaires
+// Import des routes pour l'authentification
+// Import de la configuration de la base de données
+// Initialisation de l'application Express
+// Middleware pour traiter les requêtes JSON
+// Middleware pour parser les corps de requêtes
+// Utilisation des routes pour l'authentification
+// Configuration des options cors
+// Middleware pour gerer les cors
+// Définition du port de demarrage du serveur
+// Fonction pour demarrer le serveur
+// Appel de la fonction pour demarrer le serveur
